d9ef9fb95a6d2890259bcb0be9dcbc1a9bce7d11
mgpyone <mgpyone@mysteryzillion.com>
2013-05-14 14:24:07 +0630

    Changed to File.basename instead of gsub

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ get '/' do
   redirect to ('/files')
 end
 
+# downloads to be serverd via /dl/
 get '/dl/:filename' do |file|
   file = File.join('books',file)
   send_file(file, :disposition => 'attachment')
